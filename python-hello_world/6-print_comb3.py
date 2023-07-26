3c8412928c1f2941d102f136ba6f644d14920faf
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-07-26 23:05:26 +0100

    python

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,2 @@
-for i in range(0, 8):
-    for j in range(i, 10):
-        if i != j:
-            print("{:d}{:d}, ".format(i, j), end='')
-print("89")
+# Display the massage 
+print('"Programming is like building a multilingual puzzle')
