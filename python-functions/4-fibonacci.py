9595126be4df64f1dd56de818b59a4c0d21d1a00
Bigkuez <138210720+Bigkuez@users.noreply.github.com>
2023-07-26 23:25:15 +0100

    Update 4-fibonacci.py

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci_sequence(n):
+    if n < 0:
+        return ''
+    elif n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci_sequence(n-1) + fibonacci_sequence(n-2)
+    
