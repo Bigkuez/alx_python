2a7b0f2e0e7e071623c885188577a3eedec886f4
Bigkuez <138210720+Bigkuez@users.noreply.github.com>
2023-08-03 12:03:08 +0100

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
+"""
+    Represents a square.
+"""
+class Square:
+    """
+    Represents a square.
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
+    Attributes:
+        __size (int): The size of the square.
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+    """
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
+        Args:
+            size (int, optional): The size of the square. Default is 0.
 
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
 
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
