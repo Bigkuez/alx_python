ecff8ab8fd306d7c6ffef916bca399d9daa478f5
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-08-11 02:31:13 +0100

    commit

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Represents a BaseGeometry.
+    Module represents a BaseGeometry.
 """
 class BaseGeometry:
     # ... (previous implementation of BaseGeometry)
@@ -9,28 +9,12 @@ class BaseGeometry:
         """
         Calculate the area of the geometry.
 
-        This method is not implemented in the base class and should be overridden
-        in the derived classes.
-
-        Raises:
-            Exception: Indicates that the method is not implemented.
-
-        Returns:
-            None
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
         Validate the given value as an integer and greater than 0.
-
-        Args:
-            name (str): The name of the value, used in the error message.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
@@ -41,29 +25,10 @@ class BaseGeometry:
 class Rectangle(BaseGeometry):
     """
     A class representing a rectangle.
-
-    Attributes:
-        __width (int): The width of the rectangle.
-        __height (int): The height of the rectangle.
-
-    Methods:
-        None (inherits the methods from BaseGeometry).
-
-    Example:
-        r = Rectangle(3, 5)
-        print(r)
     """
     def __init__(self, width, height):
         """
         Initialize the Rectangle instance with width and height.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than or equal to 0.
         """
         self.__width = width
         self.__height = height
