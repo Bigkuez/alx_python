a889210c7647fb97c80010f4dfaa2bff34e749fe
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-08-11 02:32:28 +0100

    commit

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module consists of a Rectangle class that
+inherits from BaseGeometry.
+"""
+BaseGeometry = __import__('5-base_geometry.py').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class that inherits from BaseGeometry."""
+    def __init__(self, width, height):
+        """Initializing the class."""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """Method that returns the area of the instance. """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Special method that returns the printable string. """
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+    
