06c80709ddc96d25b9a7e89e42713ab72335db36
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-03 15:32:27 +0100

    commit

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,11 @@ def search_states(username, password, database, search_name):
     try:
         # Connect to the MySQL server
         db = MySQLdb.connect(
-            host="localhost", port=3306, user=username, passwd=password, db=database
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
         )
 
         # Create a cursor object to interact with the database
