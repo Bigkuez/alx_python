98a93ee4f919808b145ade1423eae9fbac6c590f
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-03 14:22:56 +0100

    commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,6 +4,13 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
+    """
+    Represents a state in the states table.
+
+    Attributes:
+        id (int): An auto-generated unique integer representing the state's ID.
+        name (str): A string representing the state's name (up to 128 characters).
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
