5d7f0eea156fbbc54fa551d76bf635dbbf79a5a1
Bigkuez <138210720+Bigkuez@users.noreply.github.com>
2023-09-07 03:02:12 +0100

    Update 0-hello_route.py

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -6,10 +6,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")  # Decorator to define a route
-def home():
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
 
-
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
