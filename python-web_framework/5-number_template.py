ab7802610ab7807fe0ba585d52f04e51176cbfe7
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-08 03:21:14 +0100

    commit

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -41,7 +41,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     if isinstance(n, int):
-        return render_template('5-number.html', n=n)
+        return f'<html><body><h1>Number: {n}</h1></body></html>'
     else:
         return '', 404
 
