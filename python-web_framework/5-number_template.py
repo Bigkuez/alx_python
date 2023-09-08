0eb3befb865e3e66b46ef470f30c36ebc64216da
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-08 02:46:43 +0100

    commit

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -43,6 +43,5 @@ def number_template(n):
     else:
         return '', 404
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
