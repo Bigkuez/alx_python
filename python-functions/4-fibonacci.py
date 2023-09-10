54b4b402c091d9bcf93a1f8e8385a6af3097eabe
Bigkuez <138210720+Bigkuez@users.noreply.github.com>
2023-09-10 01:35:55 +0100

    Update 4-fibonacci.py

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,9 +1,13 @@
 def fibonacci_sequence(n):
     if n < 0:
-        return ''
+        return []
     elif n == 0:
-        return 0
-    elif n == 1 or n == 2:
-        return 1
+        return [0]
+    elif n == 1:
+        return [0, 1]
     else:
-        return fibonacci_sequence(n-1) + fibonacci_sequence(n-2)
+        fib_sequence = [0, 1]  # Initialize with the first two terms
+        for i in range(2, n + 1):
+            next_term = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_term)
+        return fib_sequence
