95c0f39810e896c9e261d3ce9a659fb7c5d9a1e0
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-14 02:02:50 +0100

    commit

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -2,7 +2,7 @@
 """
 basic flask server 
 """
-from flask import Flask , 
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -10,11 +10,9 @@ app = Flask(__name__)
 def hello_hbnb():
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
