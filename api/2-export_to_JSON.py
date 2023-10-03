ba8a49e868349d8d2570bfbeeb94b3e8835a8944
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-10-03 12:28:46 +0100

    commit

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,8 +1,19 @@
-import requests
-import json
-import sys
+# Import necessary libraries
+import requests  # Used to make HTTP requests to the API
+import json      # Used to work with JSON data
+import sys       # Used to access command-line arguments
 
 def get_employee_info(employee_id):
+    """
+    Retrieves employee information and their TODO list from a REST API,
+    then exports the data to a JSON file.
+
+    Args:
+        employee_id (int): The ID of the employee whose data is to be retrieved.
+
+    Returns:
+        None
+    """
     # Define API endpoints
     base_url = "https://jsonplaceholder.typicode.com"
     user_endpoint = f"{base_url}/users/{employee_id}"
@@ -30,7 +41,7 @@ def get_employee_info(employee_id):
         }
         tasks.append(task_info)
 
-    user_tasks = { str(user_id): tasks }
+    user_tasks = {str(user_id): tasks}
 
     with open(filename, 'w') as json_file:
         json.dump(user_tasks, json_file, indent=4)
