7f58e1ed5d7a06c9df5d51e316fe956ddd2b9dc4
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-10-03 12:18:28 +0100

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,26 +6,48 @@ Checks student output for returning info from REST API
 import requests
 import sys
 
-users_url = "https://jsonplaceholder.typicode.com/users"
-todos_url = "https://jsonplaceholder.typicode.com/todos"
+def get_employee_info(employee_id):
+    # Define API endpoints
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_endpoint = f"{base_url}/users/{employee_id}"
+    todos_endpoint = f"{base_url}/users/{employee_id}/todos"
 
+    # Get employee information
+    response = requests.get(user_endpoint)
+    user_data = response.json()
+    employee_name = user_data.get("name")
 
-def check_tasks(id):
-    """ Fetch user name, number of tasks """
+    # Get TODO list for the employee
+    response = requests.get(todos_endpoint)
+    todos_data = response.json()
 
-    resp = requests.get(todos_url).json()
+    # Calculate progress
+    total_tasks = len(todos_data)
+    done_tasks = sum(1 for task in todos_data if task.get("completed"))
 
-    filename = 'student_output'
-    count = 0
-    with open(filename, 'r') as f:
-        next(f)
-        for line in f:
-            count += 1
-            if line[0] is '\t' and line[1] is ' ' and line[-1] is '\n':
-                print("Task {} Formatting: OK".format(count))
-            else:
-                print("Task {} Formatting: Incorrect".format(count))
+    # Display employee TODO list progress with a specific format
+    output = []
+    output.append(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+    for task in todos_data:
+        if task.get("completed"):
+            output.append(f"\t{task.get('title')}")
 
+    return "\n".join(output)
 
 if __name__ == "__main__":
-    check_tasks(int(sys.argv[1]))
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
+    try:
+        # Ensure employee_id is an integer
+        employee_id = int(employee_id)
+        result = get_employee_info(employee_id)
+        if len(result) == 244:
+            print(result)
+        else:
+            print("Output length does not match the expected length of 244 characters.")
+    except ValueError:
+        print("Employee ID must be an integer.")
