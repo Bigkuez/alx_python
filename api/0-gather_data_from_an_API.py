53a0ce2ac1006620c09cbb60300ba1931ac7f840
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-10-03 12:16:48 +0100

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,41 +6,26 @@ Checks student output for returning info from REST API
 import requests
 import sys
 
-def get_employee_info(employee_id):
-    # Define API endpoints
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_endpoint = f"{base_url}/users/{employee_id}"
-    todos_endpoint = f"{base_url}/users/{employee_id}/todos"
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    # Get employee information
-    response = requests.get(user_endpoint)
-    user_data = response.json()
-    employee_name = user_data.get("name")
 
-    # Get TODO list for the employee
-    response = requests.get(todos_endpoint)
-    todos_data = response.json()
+def check_tasks(id):
+    """ Fetch user name, number of tasks """
 
-    # Calculate progress
-    total_tasks = len(todos_data)
-    done_tasks = sum(1 for task in todos_data if task.get("completed"))
+    resp = requests.get(todos_url).json()
 
-    # Display employee TODO list progress
-    print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
-    for task in todos_data:
-        if task.get("completed"):
-            print(f"\t{task.get('title')}")
+    filename = 'student_output'
+    count = 0
+    with open(filename, 'r') as f:
+        next(f)
+        for line in f:
+            count += 1
+            if line[0] is '\t' and line[1] is ' ' and line[-1] is '\n':
+                print("Task {} Formatting: OK".format(count))
+            else:
+                print("Task {} Formatting: Incorrect".format(count))
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-
-    try:
-        # Ensure employee_id is an integer
-        employee_id = int(employee_id)
-        get_employee_info(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer.")
+    check_tasks(int(sys.argv[1]))
