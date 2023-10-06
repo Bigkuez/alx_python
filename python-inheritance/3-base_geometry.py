0dd324799b8fbb13e9a984c17fd26cae0b1f5497
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-10-06 10:13:30 +0100

    commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+
+# Import the BaseGeometry class from the '3-base_geometry' module using __import__
 BaseGeometry = __import__('3-base_geometry').BaseGeometry
 
+# Create an instance of the BaseGeometry class and assign it to the 'bg' variable
 bg = BaseGeometry()
 
+# Print the instance 'bg' which is an object of the BaseGeometry class
 print(bg)
+
+# Print the list of attributes and methods available for the 'bg' object using the 'dir' function
 print(dir(bg))
+
+# Print the list of attributes and methods available for the BaseGeometry class itself
 print(dir(BaseGeometry))
