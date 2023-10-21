d324f230cb9dad11d2632b4a88dcf485358a915a
Bigkuez <138210720+Bigkuez@users.noreply.github.com>
2023-10-21 23:34:26 +0100

    commit

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,63 +1,45 @@
-# Import necessary libraries
-import requests  # Used to make HTTP requests to the API
-import json      # Used to work with JSON data
-import sys       # Used to access command-line arguments
-
-def get_employee_info(employee_id):
-    """
-    Retrieves employee information and their TODO list from a REST API,
-    then exports the data to a JSON file.
-
-    Args:
-        employee_id (int): The ID of the employee whose data is to be retrieved.
-
-    Returns:
-        None
-    """
-    # Define API endpoints
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_endpoint = f"{base_url}/users/{employee_id}"
-    todos_endpoint = f"{base_url}/users/{employee_id}/todos"
-
-    # Get employee information
-    response = requests.get(user_endpoint)
-    user_data = response.json()
-    user_id = user_data.get("id")
-    user_name = user_data.get("username")
-
-    # Get TODO list for the employee
-    response = requests.get(todos_endpoint)
-    todos_data = response.json()
-
-    # Create a JSON file for the user
-    filename = f"{user_id}.json"
-
-    tasks = []
-    for task in todos_data:
-        task_info = {
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-            "username": user_name
-        }
-        tasks.append(task_info)
-
-    user_tasks = {str(user_id): tasks}
-
-    with open(filename, 'w') as json_file:
-        json.dump(user_tasks, json_file, indent=4)
-
-    print(f"JSON file '{filename}' has been created successfully.")
+#!/usr/bin/python3
+"""
+    python script that exports data in the JSON format
+"""
+import json
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_and_export_to_json.py <employee_id>")
-        sys.exit(1)
+    """
+        request user info by employee ID
+    """
+    request_employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
 
-    employee_id = sys.argv[1]
+    user = json.loads(request_employee.text)
 
-    try:
-        # Ensure employee_id is an integer
-        employee_id = int(employee_id)
-        get_employee_info(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer.")
+    username = user.get("username")
+
+    request_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+
+    tasks = {}
+
+    user_todos = json.loads(request_todos.text)
+    """
+        loop through dictionary & get completed tasks
+    """
+    for dictionary in user_todos:
+        tasks.update({dictionary.get("title"): dictionary.get("completed")})
+
+    task_list = []
+    for k, v in tasks.items():
+        task_list.append({
+            "task": k,
+            "completed": v,
+            "username": username
+        })
+
+    json_to_dump = {argv[1]: task_list}
+    """
+        export to JSON
+    """
+    with open('{}.json'.format(argv[1]), mode='w') as file:
+        json.dump(json_to_dump, file)
