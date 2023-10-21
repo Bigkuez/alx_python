9229b3b9e46c9449da0da11574d0f055f418b7a1
Bigkuez <138210720+Bigkuez@users.noreply.github.com>
2023-10-21 23:32:31 +0100

    commit

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,58 +1,41 @@
-import os
+#!/usr/bin/python3
+"""
+    python script that exports data in the CSV format
+"""
+
 import csv
+import json
 import requests
-import sys
+from sys import argv
 
-def get_employee_info(employee_id):
-    # Define API endpoints
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_endpoint = f"{base_url}/users/{employee_id}"
-    todos_endpoint = f"{base_url}/users/{employee_id}/todos"
-
-    # Get employee information
-    response = requests.get(user_endpoint)
-    user_data = response.json()
-    user_id = user_data.get("id")
-    user_name = user_data.get("username")
-
-    # Get TODO list for the employee
-    response = requests.get(todos_endpoint)
-    todos_data = response.json()
-
-    # Create a CSV file for the user
-    filename = f"{user_id}.csv"
-
-    with open(filename, mode='w', newline='') as csv_file:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for task in todos_data:
-            writer.writerow({
-                "USER_ID": user_id,
-                "USERNAME": user_name,
-                "TASK_COMPLETED_STATUS": task.get("completed"),
-                "TASK_TITLE": task.get("title")
-            })
-
-    print("Number of tasks in CSV: OK")
-    print("User ID and Username: OK")
-    print("Formatting: OK")
-    if os.path.exists(filename):
-        print(f"CSV file '{filename}' has been created successfully.")
-    else:
-        print(f"CSV file '{filename}' was not created.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
+    """
+        request user info by employee ID
+    """
+    request_employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+    """
+        convert json to dictionary
+    """
+    user = json.loads(request_employee.text)
 
-    employee_id = sys.argv[1]
+    username = user.get("username")
 
-    try:
-        # Ensure employee_id is an integer
-        employee_id = int(employee_id)
-        get_employee_info(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer.")
+    request_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+
+    tasks = {}
+
+    user_todos = json.loads(request_todos.text)
+
+    for dictionary in user_todos:
+        tasks.update({dictionary.get("title"): dictionary.get("completed")})
+
+    """
+        export to CSV
+    """
+    with open('{}.csv'.format(argv[1]), mode='w') as file:
+        file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for k, v in tasks.items():
+            file_editor.writerow([argv[1], username, v, k])
